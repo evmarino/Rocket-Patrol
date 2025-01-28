4e52f43be3ee0384fbe6f1d012e9c32aee619ac1
evmarino <evmarino@ucsc.edu>
2025-01-27 21:56:34 -0800

    mods

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+//Evelyn Marino 
+//Rocket Patrol : On the Lookout
+
+//Approx time: 2 days (I worked in sections, not sure how many hours but about 18 maybe)
+//Mods included: Implement an alternating two-player mode (5)
+//Display the time remaining (in seconds) on the screen (3)
+//Implement mouse control for player movement and left mouse click to fire (5)
+//Allow the player to control the Rocket after it's fired (1)
+//adds time to score once hit (apart of the 5 point tier but i could only do the addition, not the subtraction for now)
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,41 +1,43 @@
-//Rocket prefab
-class Rocket extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture, frame){
-        super(scene, x,y, texture, frame)
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
 
-        
-        scene.add.existing(this) //add object to existing scene
-        this.isFiring = false // track rocket's firing status
-        this.moveSpeed = 2 // rocket speed in pixels/frame
-        this.sfxShot = scene.sound.add('sfx-shot')
-    }
-    update(){ 
-    //left right movement
-    if(!this.isFiring){
-       if(keyLEFT.isDown && this.x >= borderUISize + this.width){
-        this.x -= this.moveSpeed
-       } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-        this.x += this.moveSpeed
-       }   
-    }
-    //fire button 
-    if(Phaser.Input.Keyboard.JustDown(keyFIRE) && !this.isFiring){
-        this.isFiring = true
-        this.sfxShot.play()
-    }
-    // if fired, move up
-    if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
-        this.y -= this.moveSpeed
-    }
-    // reset on miss
-    if(this.y <= borderUISize * 3 + borderPadding){
-        this.isFiring = false
-        this.y = game.config.height - borderUISize - borderPadding
+        // Add object to the existing scene
+        scene.add.existing(this);
+
+        // Track rocket firing status
+        this.isFiring = false;
+
+        // Rocket speed
+        this.moveSpeed = 2;
     }
 
-   }
-   reset() {
-    this.isFiring = false
-    this.y = game.config.height - borderUISize - borderPadding
-   }
+    update() {
+        // If the rocket is not firing, allow movement
+        if (!this.isFiring) {
+            if (keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+
+        // If the rocket is firing, move it upward
+        if (this.isFiring) {
+            this.y -= this.moveSpeed;
+            // Reset when it goes off screen
+            if (this.y <= borderUISize * 3) {
+                this.reset();
+            }
+        }
+    }
+
+    fire() {
+        this.isFiring = true; // Rocket is now firing
+    }
+
+    reset() {
+        this.isFiring = false; // Reset firing status
+        this.y = game.config.height - borderUISize - borderPadding; // Reset to starting position
+    }
 }
